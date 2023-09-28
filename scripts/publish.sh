0b433abd561fa443274356eaf0dc798f82173de8
thebestlearner-ric <watto1990@gmail.com>
2023-09-28 22:32:36 +0800

    apk install kubectl in goagent 3

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,10 +3,10 @@
 # docker images
 # TAG_NAME=$(git rev-parse --short=8 HEAD)
 # docker push $DOCKER_REPO:backend-$TAG_NAME
-sudo apk update && \
-sudo apk add --no-cache curl && \
+apk update && \
+apk add --no-cache curl && \
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" && \
-sudo chmod +x kubectl && \
+chmod +x kubectl && \
 mv kubectl /usr/local/bin/
 
 kubectl get pod -n gocd
