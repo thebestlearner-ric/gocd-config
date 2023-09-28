b4d600d225ee3c5cd8dc1e2ab3c18dfe566418a7
thebestlearner-ric <watto1990@gmail.com>
2023-09-28 22:57:09 +0800

    try kgp in publish

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,10 +3,5 @@
 # docker images
 # TAG_NAME=$(git rev-parse --short=8 HEAD)
 # docker push $DOCKER_REPO:backend-$TAG_NAME
-apk update && \
-apk add --no-cache curl && \
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" && \
-chmod +x kubectl && \
-mv kubectl /usr/local/bin/
 
 kubectl get pod -n gocd
