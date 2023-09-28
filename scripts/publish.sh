f66fcef16c198a63182d423be7c70e7f50d401a0
thebestlearner-ric <watto1990@gmail.com>
2023-09-28 20:58:13 +0800

    move build out

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -xe
-docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
-TAG_NAME=$(git rev-parse --short=8 HEAD)
-docker push $DOCKER_REPO:backend-$TAG_NAME
+# docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
+# docker images
+# TAG_NAME=$(git rev-parse --short=8 HEAD)
+# docker push $DOCKER_REPO:backend-$TAG_NAME
+
+kubectl get pod -n gocd
