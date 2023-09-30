cd09c4635e03456e795035beaa342856146e8d1d
thebestlearner-ric <watto1990@gmail.com>
2023-09-30 11:56:51 +0800

    add commands to publish

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,7 +5,10 @@ alias k="$HOME/.local/bin/kubectl"
 k get pod -n gocd
 
 NAMESPACE="$ENV"
-
+git clone https://github.com/thebestlearner-ric/air-backend.git
+git rev-parse --short HEAD
+docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
+docker images
 if k get namespace "$NAMESPACE" &> /dev/null; then
   echo "Namespace '$NAMESPACE' already exists."
 else
