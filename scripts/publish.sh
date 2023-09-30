83e5fee9abaac1c6c6423fde1ee1df7c1c687ea4
thebestlearner-ric <watto1990@gmail.com>
2023-09-30 14:15:36 +0800

    fixing sed 3

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -22,8 +22,8 @@ echo "Deploying..."
 echo "apply deployment.yaml"
 IMAGE="$DOCKER_REPO:$TAGNAME"
 echo $IMAGE
-# sed -i "s/\$IMAGE/\$(echo "$IMAGE")/g" kube/manifest/kube/manifest/backend-deployment.yaml
-sed -i "s#$IMAGE#$(echo $IMAGE)#g" kube/manifest/backend-deployment.yaml
+sed -i "s/IMAGE/\$(echo "$IMAGE")/g" kube/manifest/backend-deployment.yaml
+# sed -i "s#$IMAGE#$(echo $IMAGE)#g" kube/manifest/backend-deployment.yaml
 echo "$(cat kube/manifest/backend-deployment.yaml)"
 $HOME/.local/bin/kubectl apply -f kube/manifest/backend-deployment.yaml -n $ENV
 echo "apply service.yaml"
