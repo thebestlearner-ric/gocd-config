42c9e26cf7394c75d186f5cd522f67175f7aced7
thebestlearner-ric <watto1990@gmail.com>
2023-09-30 14:51:05 +0800

    fixing sed

--- scripts/deploy-backend.sh
+++ scripts/deploy-backend.sh
@@ -21,7 +21,7 @@ echo "what is in kube manifest $(ls -altr kube/manifest/)"
 echo "Deploying..."
 echo "apply deployment.yaml"
 echo $TAGNAME
-sed -i "s/TAG/\$(echo "$TAGNAME")/g" kube/manifest/backend-deployment.yaml
+sed -i "s/TAG/$(echo $TAGNAME)/g" kube/manifest/backend-deployment.yaml
 # sed -i "s#$IMAGE#$(echo $IMAGE)#g" kube/manifest/backend-deployment.yaml
 echo "$(cat kube/manifest/backend-deployment.yaml)"
 $HOME/.local/bin/kubectl apply -f kube/manifest/backend-deployment.yaml -n $ENV
