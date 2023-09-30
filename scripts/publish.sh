17da828cf241a11a46d13b3a6366f79808c18029
thebestlearner-ric <watto1990@gmail.com>
2023-09-30 12:48:53 +0800

    attempt to kubectl deploy 2

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -20,10 +20,10 @@ else
 fi
 
 echo "what is in kube manifest $(ls -altr kube/manifest/)"
-echo "$(cat kube/manifest/backend-deployment.yaml)"
 echo "Deploying..."
 echo "apply deployment.yaml"
-
-$HOME/.local/bin/kubectl apply -f kube/manifest/backend-deployment.yaml
+sed -i "s/\$IMAGE/$(echo $IMAGE)/g" kube/manifest/backend-deployment.yaml
+echo "$(cat kube/manifest/backend-deployment.yaml)"
+$HOME/.local/bin/kubectl apply -f kube/manifest/backend-deployment.yaml -n $ENV
 echo "apply service.yaml"
-$HOME/.local/bin/kubectl apply -f kube/manifest/backend-service.yaml
+$HOME/.local/bin/kubectl apply -f kube/manifest/backend-service.yaml -n $ENV
